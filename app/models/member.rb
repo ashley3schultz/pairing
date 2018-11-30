d9a5d4115b21a9251d41dfbc48bee4c13bc959f7
ashley3schultz <ashleyeschultz@gmail.com>
2018-11-30 08:40:16 -0700

    Change member model and table

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ApplicationRecord
+    has_and_belongs_to_many(:members,
+    :join_table => "member_connections",
+    :foreign_key => "member_a_id",
+    :association_foreign_key => "member_b_id")
 end
