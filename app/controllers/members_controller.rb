20d644733ceeac38e4b105d3933590cd1bd6d604
ashley3schultz <ashleyeschultz@gmail.com>
2018-12-05 19:38:13 -0700

    Update member controller to add index and show

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,12 @@
 class MembersController < ApplicationController
+
+    def index 
+        @members = Member.all
+    end
+
+    def show 
+        @member = Member.find_by(id: params[:id])
+        @pairs = Member.pairs(@member.id)
+    end 
+
 end
