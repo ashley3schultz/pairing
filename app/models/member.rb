31b26fd70c0b68a11b5002bade34ce95900d03e6
ashley3schultz <ashleyeschultz@gmail.com>
2018-12-05 19:39:35 -0700

    Add model helper for finding pairs related to member

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,18 @@ class Member < ApplicationRecord
     :join_table => "member_connections",
     :foreign_key => "member_a_id",
     :association_foreign_key => "member_b_id")
+
+    def self.pairs(id)
+        pairs = []
+        MemberConnection.where(member_a_id: id).each do |m| 
+            mem = Member.find(m.member_b_id)
+            pairs << mem 
+        end
+        MemberConnection.where(member_b_id: id).each do |m| 
+            mem = Member.find(m.member_a_id)
+            pairs << mem
+        end 
+        pairs
+    end 
+
 end
