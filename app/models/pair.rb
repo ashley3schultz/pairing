edc530ebf74921a412ff15c51180aeac6c54b5b6
ashley3schultz <ashleyeschultz@gmail.com>
2018-12-23 21:53:50 -0700

    Update method in pairs model to fix redundant pairs

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -3,24 +3,49 @@ class Pair < ApplicationRecord
     belongs_to :member_b, :class_name => :Member
 
     def self.create_pairs
-        if Member.all.first.pairs.size >= Member.where(active: true).size
-            self.delete_all
-        end
+        self.check
         members = Member.where(active: true).shuffle
+
+        pairs = []
         spoken_for = []
-        @pairs = []
-        binding.pry
-        members.each do |m|
-            i = 0
-            a = m != members[i]
-            b = m.pairs.include?(!members[i])
-            c = spoken_for.include?(!members[i])
-            until (a && b && c) do
-                i+= 1
+
+        members.each do |ma|
+            ma.pairable
+            mb = ma.pairable.detect{|m| !spoken_for.include?(member) || !spoken_for.include?(ma) }
+            if mb
+                pairs << self.new(member_a_id: ma.id, member_b_id: mb.id, current: true)
+                spoken_for << ma
+                spoken_for << mb
+            else 
+                pairs.detect do |p| 
+                    bp = p.member_b.pairable.include?(members.select{|mem| !spoken_for.include?(mem)}
+                    ap = p.member_a.pairable.include?(members.select{|mem| !spoken_for.include?(mem)}
+                    if ma.pairable.include?(p.member_a) && bp
+                        mb = bp.first
+                        p.member_b = ma.id
+                        spoken_for << ma
+                        pairs << self.new(member_a_id: mb.id, member_b_id: p.member_b.id, current: true)
+                    elsif ma.pairable.include?(p.member_b) && ap
+                            mb = ap.first
+                            p.member_a = ma.id
+                            spoken_for << ma
+                            pairs << self.new(member_a_id: mb.id, member_b_id: p.member_b.id, current: true)
+                    else 
+                        
             end
-            @pairs << self.new(member_a_id: m.id, member_b_id: member[i].id)
-            spoken_for << members[i]
-            spoken_for << m
         end
     end
+
+    def self.check
+        num = 0
+        Member.all.each do |mem| 
+            num = mem.pairs.size if num < mem.pairs.size 
+        end
+        if num >= Member.where(active: true).size - 1
+            Pair.delete_all
+        else 
+            update_all(current: false)
+        end
+    end
+
 end
