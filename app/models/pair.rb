0712503550e9eac3483801162bf4589bd17e6f6e
ashley3schultz <ashleyeschultz@gmail.com>
2018-12-30 22:52:46 -0700

    Seperate method create_pairs

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -2,36 +2,24 @@ class Pair < ApplicationRecord
     belongs_to :member_a, :class_name => :Member
     belongs_to :member_b, :class_name => :Member
 
+    @@members = []
+    @@pairs = []
+    @@paired = []
+
+   
+
     def self.create_pairs
         self.check
         members = Member.where(active: true).shuffle
-
-        pairs = []
-        spoken_for = []
-
-        members.each do |ma|
-            ma.pairable
-            mb = ma.pairable.detect{|m| !spoken_for.include?(member) || !spoken_for.include?(ma) }
-            if mb
-                pairs << self.new(member_a_id: ma.id, member_b_id: mb.id, current: true)
-                spoken_for << ma
-                spoken_for << mb
-            else 
-                pairs.detect do |p| 
-                    bp = p.member_b.pairable.include?(members.select{|mem| !spoken_for.include?(mem)}
-                    ap = p.member_a.pairable.include?(members.select{|mem| !spoken_for.include?(mem)}
-                    if ma.pairable.include?(p.member_a) && bp
-                        mb = bp.first
-                        p.member_b = ma.id
-                        spoken_for << ma
-                        pairs << self.new(member_a_id: mb.id, member_b_id: p.member_b.id, current: true)
-                    elsif ma.pairable.include?(p.member_b) && ap
-                            mb = ap.first
-                            p.member_a = ma.id
-                            spoken_for << ma
-                            pairs << self.new(member_a_id: mb.id, member_b_id: p.member_b.id, current: true)
-                    else 
-                        
+        members.each do |a|
+            a.pairable
+            if !@@paired.include?(a)
+                b = a.pairable.detect{|p| !@@paired.include?(p) }
+                if b
+                    self.add_pair(a, b)
+                else
+                    self.steal_pair(a)
+                end
             end
         end
     end
