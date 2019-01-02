ba4dfebb593012c9dbdc49069d7649ca4cc22ed2
ashley3schultz <ashleyeschultz@gmail.com>
2019-01-01 18:53:12 -0700

    Update steel pair

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -6,13 +6,25 @@ class Pair < ApplicationRecord
     @@pairs = []
     @@paired = []
 
+    def self.create_pairs
+        self.prepair
+        @@members.each do |a|
+            if !@@paired.include?(a)
+                b = a.pairable.detect{|p| !@@paired.include?(p) }
+                self.add_pair(a, b) if b
+                self.steel_pair(a) if !b
+            end
+        end
+        self.save_pairs
+    end
+
     def self.add_pair(a, b)
         @@pairs << self.new(member_a_id: a.id, member_b_id: b.id, current: true)
         @@paired << a
         @@paired << b
     end 
 
-    def self.steal_pair(a)
+    def self.steel_pair(a)
         np = @@members.select{|mem| !@@paired.include?(mem)}
         @@pairs.detect do |p|
             bp = p.member_b.pairable.detect{ |mem| np.include?(mem)}
@@ -20,53 +32,45 @@ class Pair < ApplicationRecord
             if (a.pairable.include?(p.member_a) && bp) || (a.pairable.include?(p.member_b) && ap)
                 if a.pairable.include?(p.member_a) && bp
                     self.add_pair(bp, p.member_b)
+                    binding.pry
                     p.member_b = a.id
                     @@paired << a
                 else
+                    binding.pry
                     self.add_pair(ap, p.member_a)
                     p.member_a = a.id
                     @@paired << a
                 end
             else 
-                @@pairs.each do |p| 
-                    new_set = []
-                    if p.member_a.pairable.include?(p.member_a) && p.pairable.include?()
-                        new_set << self.new(member_a_id: a.id, member_b_id: p.member_a.id, current: true)
-                    else 
-                        new_set << p
-                    end
-                    @@pairs = new_set
-                end
+                # pairs = @@pairs
+                # pairs.each do |pair| 
+                #     new_set = []
+                #     if p.member_a.pairable.include?(pair.member_a) && p.member_b.pairable.include?(pair.member_b)
+                #         new_set << self.new(member_a_id: a.id, member_b_id: p.member_a.id, current: true)
+                #     else 
+                #         new_set << p
+                #     end
+                # end
+                # @@pairs = new_set
             end
         end
     end 
 
-    def self.create_pairs
-        self.check
-        members = Member.where(active: true).shuffle
-        members.each do |a|
-            a.pairable
-            if !@@paired.include?(a)
-                b = a.pairable.detect{|p| !@@paired.include?(p) }
-                if b
-                    self.add_pair(a, b)
-                else
-                    self.steal_pair(a)
-                end
-            end
-        end
-    end
-
-    def self.check
+    def self.prepair
+        @@members = Member.where(active: true).shuffle
         num = 0
-        Member.all.each do |mem| 
+        @@members.each do |mem| 
             num = mem.pairs.size if num < mem.pairs.size 
         end
-        if num >= Member.where(active: true).size - 1
+        if num >= @@members.size - 1
             Pair.delete_all
         else 
             update_all(current: false)
         end
+    end
+
+    def self.save_pairs
+        @@pairs.each {|p| p.save}
     end
 
 end
