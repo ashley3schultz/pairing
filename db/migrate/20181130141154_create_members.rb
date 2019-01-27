a3b8c79139724c1e3acb3b95231c61d7e03f7719
ashley3schultz <ashleyeschultz@gmail.com>
2019-01-26 17:32:17 -0700

    Change member migration

--- db/migrate/20181130141154_create_members.rb
+++ db/migrate/20181130141154_create_members.rb
@@ -2,9 +2,9 @@ class CreateMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :members do |t|
       t.string :name
-      t.boolean :active
-      t.integer :m_num, default: 0
-      t.integer :s_num, default: 0
+      t.boolean :active, default: true
+      t.integer :m_num, default: -1
+      t.integer :p_num, default: -1
       t.timestamps
     end
   end
