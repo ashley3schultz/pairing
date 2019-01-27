c01aea508a248bc89ba9dcdd67724444a8079ba0
ashley3schultz <ashleyeschultz@gmail.com>
2019-01-26 17:31:41 -0700

    Change pairs migration

--- db/migrate/20181130144942_create_pairs.rb
+++ db/migrate/20181130144942_create_pairs.rb
@@ -3,8 +3,7 @@ class CreatePairs < ActiveRecord::Migration[5.2]
     create_table :pairs, :id => false do |t|
       t.integer :member_a_id
       t.integer :member_b_id
-      t.boolean :current
-      t.integer :pair_index
+      t.boolean :current, default: true
       t.timestamps
     end
   end
